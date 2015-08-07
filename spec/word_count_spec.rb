89ca6ea4070fd43fa562a024ba66ed4c57beb379
Alexa <amzeazas@comcast.net>
2015-08-07 09:22:34 -0700

    Add spec for handling all uppercase entries.

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,7 @@ describe('String#word_count') do
   it("counts the number of times a word appears in a multiple word string") do
     expect(("what is my name").word_count("is")).to(eq(1))
   end
+  it("handles all uppercase entries") do
+    expect(("WHAT IS MY NAME").word_count("is")).to(eq(1))
+  end
 end

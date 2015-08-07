4e1966ae33152b7dd77ab847aa6a9f64a7f2a9bc
Alexa <amzeazas@comcast.net>
2015-08-07 08:50:32 -0700

    Add spec for counting number of times a word appears in a multiple word string.

---
+++ spec/word_count_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('word_count')
+
+describe('String#word_count') do
+  it("counts the number of times a word appears in a multiple word string") do
+    expect(("what is my name").word_count("is")).to(eq(1))
+  end
+end

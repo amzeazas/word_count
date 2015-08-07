426334874766beecb23bca14c59926b43a8ef6a3
Alexa <amzeazas@comcast.net>
2015-08-07 09:44:56 -0700

    Add form into index.erb file.

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -7,8 +7,8 @@ set(:show_exceptions, false)
 describe('the word count path', {:type => :feature}) do
   it('processes the user entry and returns the number of times a string appears in a multiple word string') do
     visit('/')
-    fill_in('word', :with => 'name')
     fill_in('compare', :with => 'my name is name')
+    fill_in('word', :with => 'name')
     click_button('Count!')
     expect(page).to have_content('"name" shows up in "my name is name" 2 times')
   end

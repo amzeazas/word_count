bc69ef5111bbfb363c76d59b7f76d6f3ccda1ada
Alexa <amzeazas@comcast.net>
2015-08-07 09:23:38 -0700

    Add code for handling all uppercase entries.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,9 +1,10 @@
 class String
   define_method(:word_count) do |target_word|
-    new_string = self.scan(/\w+/)
+    new_string = self.downcase.scan(/\w+/)
+    target_word = target_word.downcase()
 
     new_array = new_string.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
-    
+
     final_count = new_array.fetch(target_word)
     final_count
   end

ac2d0e2d62db9a5a397141d227b6c62de9ab686a
Alexa <amzeazas@comcast.net>
2015-08-07 09:47:28 -0700

    Add /count path to app.rb file.

--- app.rb
+++ app.rb
@@ -6,3 +6,10 @@ also_reload('lib/**/*.rb')
 get('/') do
   erb(:index)
 end
+
+get('/count') do
+  @word = params.fetch('word')
+  @compare = params.fetch('compare')
+  @count = params.fetch('compare').word_count(params.fetch('word'))
+  erb(:count)
+end
